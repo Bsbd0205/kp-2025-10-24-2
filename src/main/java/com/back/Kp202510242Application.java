c08ad54033cedb1ed82e326e6008525302bb7bf2
bsbd0205 <ggcw0205@dankook.ac.kr>
2025-10-24 14:01:54 +0900

    first commit

---
+++ src/main/java/com/back/Kp202510242Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kp202510242Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kp202510242Application.class, args);
+    }
+
+}
